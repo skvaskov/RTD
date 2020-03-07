c3a8d2e2a3705b1c4646fd9b162ec3e89c573adf
Shreyas Kousik <skousik@umich.edu>
2020-03-07 16:20:16 -0500

    removed simulator temporarily

--- simulator_files/src/utility/geometry/shape_creation/make_random_unit_vector.m
+++
@@ -1,7 +0,0 @@
-function u = make_random_unit_vector(D)
-% u = make_random_unit_vector(D)
-%
-% Make random unit vector of dimension D (integer).
-    u = 2.*rand(D,1) - 1 ;
-    u = u./norm(u) ;
-end
--- simulator_files/src/utility/timing/error_if_out_of_time.m
+++
@@ -1,5 +0,0 @@
-function error_if_out_of_time(start_tic, timeout)
-    if toc(start_tic) > timeout
-        error('Out of time!')
-    end
-end
