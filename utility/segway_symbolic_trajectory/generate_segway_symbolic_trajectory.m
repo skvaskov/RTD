749c75aaf9b16aa98782be8a384764ea5cb7e16e
Shreyas Kousik <skousik@umich.edu>
2020-03-25 10:56:40 -0400

    updated segway symbolic trajectory to RK4

--- utility/segway_symbolic_trajectory/generate_segway_symbolic_trajectory.m
+++ utility/segway_symbolic_trajectory/generate_segway_symbolic_trajectory.m
@@ -4,9 +4,14 @@
 % an intial yaw rate and velocity. The trajectory has an initial state of
 % (x,y,h,w,v) = (0,0,0,w_0,v_0).
 %
+% This takes about 1 -- 2 s to precompute the symbolic trajectory with
+% RK4 integration. Generating a MATLAB function handle from the symbolic
+% trajectory takes about 2 s. Saving a MATLAB file of that function takes
+% a few minutes.
+%
 % Author: Shreyas Kousik
 % Created: 24 Mar 2020
-% Updated: nah
+% Updated: 25 Mar 2020
 %
 %% user parameters
 % trajectory timing
@@ -20,7 +25,7 @@ w_des_val = -1 ;
 v_des_val = 0.25 ;
 
 % save symbolic traj
-save_flag = false ;
+save_flag = true ;
 data_filename = 'segway_symbolic_traj_data.mat' ;
 traj_filename = 'segway_symbolic_traj.m' ;
 
@@ -36,21 +41,38 @@ z_0 = [zeros(3,1) ; w_0 ; v_0] ;
 N_T = length(T) ;
 Z_sym = [z_0, nan(5,N_T-1)] ;
 
+disp('Computing symbolic trajectory')
 tic
 for idx = 2:N_T
-    % euler-integrate to get new state
+    % get old state
     z_old = Z_sym(:,idx-1) ;
-    z_new = z_old + dt_traj.*segway_symbolic_model(z_old,[w_des;v_des]) ;
+    
+    % RK4 to get new state
+    h = dt_traj ;
+    k_1 = h*segway_symbolic_model(z_old,[w_des;v_des]) ;
+    k_2 = h*segway_symbolic_model(z_old + k_1/2,[w_des;v_des]) ;
+    k_3 = h*segway_symbolic_model(z_old + k_2/2,[w_des;v_des]) ;
+    k_4 = h*segway_symbolic_model(z_old + k_3,[w_des;v_des]) ;
+    
+    z_new = z_old + (1/6)*(k_1 + 2*k_2 + 2*k_3 + k_4) ;
+    
+    % save new state
     Z_sym(:,idx) = z_new ;
 end
 toc
 
 % generate matlab function from symbolic dynamics
 if save_flag
+    disp('Saving trajectory function as MATLAB file')
+    tic
     Z_fn = matlabFunction(Z_sym,'Vars',{'w_0','v_0','w_des','v_des'},...
         'File',traj_filename) ;
+    toc
 else
+    disp('Generating trajectory function')
+    tic
     Z_fn = matlabFunction(Z_sym,'Vars',{'w_0','v_0','w_des','v_des'}) ;
+    toc
 end
 
 %% save the timing info
