a8b800595ac1eb89ea35045c2d63e3912e826705
Sean Vaskov <skvaskov@umich.edu>
2021-08-03 02:30:05 -0400

    Update sosOnK.m

--- utility/spotless/sosOnK.m
+++ utility/spotless/sosOnK.m
@@ -6,6 +6,7 @@
 % h   -- m-by-1 msspoly in x. (semialgebraic constraints defining K)
 % d   -- scalar integer, d > deg(g).
 % b   -- cell array of subsets of x
+% g   -- l-by-1 msspoly in x. (equality constraints defining K)
 %
 % prgout --  new program with constraints
 %       s(i) SOS, p - s'*h SOS
@@ -23,7 +24,11 @@
 %     [ prg, tk ] = prg.withSOS( p - S'*h ); % ensure that p is SOS on K 
 % end
 
-function [ prg, tk ] = sosOnK( prg, p, x, h, d, b )
+function [ prg, tk ] = sosOnK( prg, p, x, h, d, b,g)
+if nargin < 7
+    g = [];
+end
+
 if isa(x,'msspoly')
     deg_p= max( mod(deg(p,x),2) + deg(p,x), d);
 else
@@ -33,7 +38,7 @@ end
     S = msspoly( zeros( m, 1 ) ); % create this many multiplier variables
     for i = 1:m
         deg_h=deg(h(i),x);
-        if nargin < 6
+        if (nargin < 6) || isempty(b)
             bases = monomials( x, 0:max(0, mod(deg_p - deg_h,2) + (deg_p - deg_h)) );
         else
             bases = [] ;
@@ -45,11 +50,40 @@ end
         [ prg, S( i ) ] = prg.newFreePoly( bases ); % make a polynomial whose coefficients are free variables
         prg = prg.withSOS( S( i ) ); % ensure that the polynomial is SOS
     end
-%     keyboard
-if ~isempty(S)
-    [ prg, tk ] = prg.withSOS( p - S'*h ); % ensure that p is SOS on K 
-else
-    [ prg, tk ] = prg.withSOS( p ); % ensure that p is SOS on K 
+%     prg.sosTokens=[prg.sosTokens;tk]; % storing the tokens corresponding to the desired SOS constraint
+
+l = size( g, 1 ); % total number of g's
+
+R = msspoly( zeros( l, 1 ) ); % create this many multiplier variables
+for i = 1:l
+    deg_g=deg(g(i),x);
+    if nargin < 6 || isempty(b)
+        bases = monomials( x, 0:max(0, deg_p - deg_g));
+    else
+        bases = [] ;
+        for bidk = 1:length(b)
+            bases = [bases ; monomials( b{bidx}, 0:max([0, deg_p-deg_g]) ) ] ;
+        end
+    end
+    [ prg, R( i ) ] = prg.newFreePoly( bases ); % make a polynomial whose coefficients are free variables
 end
+
+% keyboard
+if isempty(S)
+    S = 0;
+end
+if isempty(R)
+    R = 0;
+end
+if m == 0
+    h = 0;
+end
+if l == 0
+    g = 0;
+end
+
+[ prg, tk ] = prg.withSOS( p - S'*h - R'*g ); % ensure that p is SOS on K
+
 %     prg.sosTokens=[prg.sosTokens;tk]; % storing the tokens corresponding to the desired SOS constraint
 end
+
