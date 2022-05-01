137e778c1a4de558ca439116661a7c9f1cd2ac31
Shreyas Kousik <shreyas.kousik@gmail.com>
2022-05-01 12:01:53 -0700

    Create is_RTD_on_path.m

---
+++ utility/is_RTD_on_path.m
@@ -0,0 +1,8 @@
+function is_RTD_on_path()
+% is_RTD_on_path()
+%
+% If the RTD repo is on your MATLAB path, then this function can be called
+% and will not throw an error.
+
+    disp('The RTD repo is on your MATLAB path!')
+end
